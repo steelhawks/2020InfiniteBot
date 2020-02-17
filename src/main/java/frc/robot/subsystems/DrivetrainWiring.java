8033a1e0cfed7754650a3a6cad0e923f2fa3d011
Weijian Zeng <weijian@prosparency.com>
2020-02-17 10:11:42 -0500

    add wiring class for drivetrain

---
+++ src/main/java/frc/robot/subsystems/DrivetrainWiring.java
@@ -0,0 +1,31 @@
+/*----------------------------------------------------------------------------*/
+/*   Configuration Class to manage port configurations                        */
+/*   and motor groupings                                                      */
+/*                                                                            */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+public final class DrivetrainWiring {
+
+  /*****
+   * Drivetrain
+   */
+
+  public final int drivetrainLeftMotorOnePort = 1;
+  public final int drivetrainLeftMotorTwoPort = 2;
+  public final int drivetrainLeftMotorThreePort = 3;
+
+  public final int drivetrainRightMotorOnePort = 6; //4;
+  public final int drivetrainRightMotorTwoPort = 5;
+  public final int drivetrainRightMotorThreePort = 4; //6;
+
+  public final int drivetrainSolOnPort = 0;
+  public final int drivetrainSolOffPort = 1;
+
+  public final int falconSolOnPort = 2;
+  public final int falconSolOffPort = 3;
+
+
+}
